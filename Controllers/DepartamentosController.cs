35a548b0736b9a1a9870889a7c7d69bf04b66267
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-09 11:12:56 +0200

    Add project files.

---
+++ Controllers/DepartamentosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcClienteApiCrudDepartamentos.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+    }
+}
